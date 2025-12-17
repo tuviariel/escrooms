92b78cc8abfba2607246debaa8b36fdf6d3c1580
Tuvia Ariel <tuviaa109@gmail.com>
2025-12-17 12:10:00 +0200

    last TS fix for now

--- src/components/Language/Language.tsx
+++ src/components/Language/Language.tsx
@@ -5,7 +5,7 @@ export const Language = () => {
     return (
         <div className="flex items-start my-2" dir={userLanguage === "he" ? "rtl" : "ltr"}>
             <h6>{get_text("language", userLanguage)}:</h6>
-            <select value={userLanguage} onChange={(e) => setUserLanguage(e.target.value)}>
+            <select value={userLanguage} onChange={(e) => setUserLanguage(e.target.value as typeof userLanguage)}>
                 <option value="en">{get_text("english", userLanguage)}</option>
                 <option value="he">{get_text("hebrew", userLanguage)}</option>
                 {/* <option value="ar">{get_text("arabic", userLanguage)}</option> */}
